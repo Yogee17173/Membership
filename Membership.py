82a063edd16f0616f47ef489439dfa705939594e
YOGEESH K P <yogeesh793@gmail.com>
2025-02-21 23:05:55 +0530

    Add files via upload

---
+++ Membership.py
@@ -0,0 +1,69 @@
+# Python Membership Operators
+Fruits = ["apple", "banana", "cherry"]
+if "banana" in Fruits:
+  print("Yes, 'banana' is in the fruits list")
+else:
+  print("No, 'banana' is not in the fruits list")
+  
+# 2
+Mobile = ["Iphone", "Samsung", "Nokia"]
+if "Nokia" in Mobile:
+  print("yes, 'Nokia' is in Mobile") 
+else:
+  print("No, 'Nokia' is not in Mobile")
+
+#3
+Form = ["cow", "cattle", "bufellow"]
+if "cattle" in Form:
+  print("Yes, 'cattle' is in Form")
+else:
+  print("No, 'cattle' is not in Form")
+
+#4
+Car = ["BMW", "Audi", "Mercedes"]
+if "Audi" in Car:
+  print("Yes, 'Audi' is in Car")
+else:
+  print("No, 'Audi' is not in Car")
+
+#5
+Bike = ["yamaha", "honda", "suzuki"]
+if "hero" in Bike:
+  print("Yes, 'honda' is in Bike")
+else:
+  print("No, 'honda' is not in Bike")
+
+#6
+Forest = ["Elephant", "Deer", "Tiger"]
+if "Deer" in Forest:
+  print("Yes, 'Deer' is in Forest")
+else:
+  print("No, 'Deer' is not in Forest")
+
+#7
+Book =["Science", "Math", "English"]
+if "Math" in Book:
+  print("Yes, 'Math' is in Book")
+else:
+  print("No, 'Math' is not in Book")
+
+#8
+Pen = ["Cello", "Fusion", "Parker"]
+if "Parker" in Pen:
+  print("Yes, 'Parker' is in Pen")
+else:
+  print("No, 'Parker' is not in Pen")
+
+#9
+Color = ["Red", "Blue", "Green"]
+if "Green" in Color:
+  print("Yes, 'Green' is in Color")
+else:
+  print("No, 'Green' is not in Color")
+
+#10
+Bus = ["TATA", "Ashok Layland", "Benze"]
+if "Ashok Layland" in Bus:
+  print("Yes, 'Ashok Layland' is in Bus")
+else:
+  print("No, 'Ashok Layland' is not in Bus")
